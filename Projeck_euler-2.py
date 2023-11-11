91145a4d3e538be6070c4dee7eae214e201b6c37
b12halflife <74808481+b12halflife@users.noreply.github.com>
2023-11-11 17:36:38 +0700

    Add files via upload

---
+++ Projeck_euler-2.py
@@ -0,0 +1,20 @@
+#projeck euler 2
+"""
+Setiap suku baru dalam deret Fibonacci dihasilkan dengan menambahkan dua suku sebelumnya. Dengan memulai dengan 1 dan 2, 10 suku pertama adalah:
+1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+Dengan mempertimbangkan suku-suku dalam barisan Fibonacci yang nilainya tidak melebihi empat juta, temukan jumlah suku-suku yang bernilai genap.
+
+"""
+limit = 4000000  # batas dari deret fibonaci
+a = 2     #
+b = 8     # agar lebih cepat kita mengunkan 2,8,34 karna bernilai genap
+c = 34    #
+sum = 10
+while c < limit:
+  sum += c  
+  a = b
+  b = c
+  c = 4 * b + a
+  print(a,b,c)
+
+print("hasil = ",sum)
